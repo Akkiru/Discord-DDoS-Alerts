c633a0c968d3b62e0a4a11464ce4715d63da6884
Akkiru <78254079+Akkiru@users.noreply.github.com>
2021-01-30 20:06:46 +1100

    Uploading The Files

---
+++ README.md
@@ -0,0 +1,24 @@
+# DDoS-Alerts
+Checks If Your Server Is Being D/DoSed And Sends A Notification To A Discord Webhook With How Much MB Is Being Hit. 
+
+
+# Installation
+
+apt install tcpdump
+
+chmod +x log.sh
+
+./log.sh
+
+
+# Credit
+My Old Fuckin Account.
+
+
+# Tags to help people find this
+DDOS ATTACK LOGGER
+Ddos logger
+discord ddos logger
+ddos alert
+discord webhook ddos alert
+ddos logger discord
---
+++ log.sh
@@ -0,0 +1,31 @@
+clear
+
+ 	interface=eth0 ## Change To Your network Interface
+ 	dumpdir=/tmp/
+
+ 		while /bin/true; do
+ 		pkt_old=`grep $interface: /proc/net/dev | cut -d : -f2 | awk '{ print $2 }'`
+ 		sleep 1
+ 		pkt_new=`grep $interface: /proc/net/dev | cut -d : -f2 | awk '{ print $2 }'`
+
+ 		pkt=$(($pkt_new-$pkt_old))
+ 			echo -ne "\r$pkt packets/s\033[0K"
+ 			if [ $pkt -gt 1024 ]; then  ## If Over 10 Megabytes / 80 Mbit Per Sec, Alerts Discord Webhook
+ 			echo -e "\n`date` Under attack, dumping packets." 
+ 			python3 msg.py ## Shows Main MSG Abt Webserver Being Hit
+ 		
+ 		pktT=$(($pkt/"10"))
+ 		
+ 		msg_content=\"Dumping" "$pktT" "Megabytes\" ## Sends The Msg About It Being Hit
+ 		
+ 		url='https://discordapp.com/api/webhooks/PUT WEBHOOK HERE' ## Insert Discord Webhook Here
+ 		
+ 		curl -H "Content-Type: application/json" -X POST -d "{\"content\": $msg_content}" $url ## Shows How Much Power Being Put Through
+ 	tcpdump -n -s0 -c 2000 -w $dumpdir/dump.`date +"%Y%m%d-%H%M%S"`.cap ## Dumps All Invalid Connected IP's And Forced Connections
+ 
+ echo "`date` Just Got Hit [Sleeping For 1 Min]."
+ 
+ sleep 60
+ 
+ fi
+done
